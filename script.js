dbdb5197cb103289125abd6613b5c6a3d9698597
Lucas Baffi <lucasrenatobaffi@hotmail.com>
2023-05-13 18:11:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const modal = () => {
+    let numero1 =22
+    let numero2 =44
+    let soma = numero1 + numero2
+    return soma
+};
+
+console.log(modal());
+
+const html = () => {
+
+ 
+
+    const div = document.createElement("div")
+    const h1 = document.createElement("h1")
+    const img = document.createElement("img")
+    const p = document.createElement("p")
+    const p2 = document.createElement("p")
+    const a = document.createElement("a")
+
+    h1.innerText = "Lucas"
+    img.src = "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png"
+    p.innerText = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequunturoptio dolores ullam ipsum sequi iure ut explicabo deserunt, facere    numquam id doloribus dolor temporibus, dicta nisi.Veniam commodi"
+    p2.innerText = "Para mais informacoes"
+    a.href = "https://www.linkedin.com/in/lucas-baffi-270636b2/"
+    a.innerText = "Linkedin"
+    let body = document.querySelector(".body")
+    div.append(h1, img, p, p2, a)
+    body.appendChild(div)
+
+    return body
+};
+html()
+
+
+
+// const renderJob = (cardList) => {
+
+//     body.innerHTML = ""
+  
+//     cardList.forEach((element) => {
+//       const card = html(element)
+//       section.appendChild(card)
+//     })
+  
+  
+//   }
+//   renderJob(jobsData)
+
